802614d26477d20102f8b156f77e085efa8d8f89
Stevie <steviescripts52@gmail.com>
2023-04-05 18:05:41 -0700

    add heroku-postgres

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///maestro'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///maestro')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
